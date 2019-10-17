5bb66701020dd7d2a3a9f7961ca51f72a5d71bbb
ProjetInfoAlexandre <apytio3@gmail.com>
2019-10-17 08:51:07 +0200

    First commit: Here we go.

---
+++ src/main/java/fr/polytech/sixnez/SixnezApplication.java
@@ -0,0 +1,13 @@
+package fr.polytech.sixnez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixnezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SixnezApplication.class, args);
+	}
+
+}
